7a8e2f01bb518cf2ac5e2c5559df4e90e0c59807
Jiwei-Cao <jiweicao2006@gmail.com>
2025-06-20 10:30:26 +0100

    fixed load model

--- model/model.py
+++ model/model.py
@@ -1,14 +1,22 @@
 import torch 
-from torchvision import models
+from torchvision import models, transforms
 from pathlib import Path
+from PIL import Image
 
-def load_model():
-    model = models.efficientnet_b2(weights=None)
-    model.classifier[1] = torch.nn.Linear(in_features=1408, out_features=6)
+BASE_DIR = Path(__file__).resolve().parent
+MODEL_PATH = BASE_DIR / "reyclo_model.pth"
+class_names = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
 
-    model_path = Path(__file__).resolve().parent / "recyclo_model.pth"
-    model.load_state_dict(torch.load(model_path, map_location=torch.device("cpu")))
+transform = transforms.Compose([
+    transforms.Resize((260, 260)),
+    transforms.toTensor()
+])
+
+def load_model(model_path=MODEL_PATH):
+    model = torch.load(model_path, map_location=torch.device("cpu"))
     model.eval()
 
     return model
     
+model = load_model()
+
