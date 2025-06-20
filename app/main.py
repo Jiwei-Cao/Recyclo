7a96b4fb1b9b6f31e25288a32a0054576951952b
Jiwei-Cao <jiweicao2006@gmail.com>
2025-06-20 10:40:32 +0100

    Debugged Model

---
+++ app/main.py
@@ -0,0 +1,3 @@
+from fastapi import FastAPI 
+from pydantic import BaseModel
+from model.model import predict, __version__ as model_version
--- model/model.py
+++ model/model.py
@@ -3,6 +3,8 @@ from torchvision import models, transforms
 from pathlib import Path
 from PIL import Image
 
+__version__ = "0.1.0"
+
 BASE_DIR = Path(__file__).resolve().parent
 MODEL_PATH = BASE_DIR / "reyclo_model.pth"
 class_names = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
@@ -20,3 +22,12 @@ def load_model(model_path=MODEL_PATH):
     
 model = load_model()
 
+def predict(image_path: str) -> str:
+    image = image.open(image_path).convert("RGB")
+    input_tensor = transform(image).unsqueeze(0)
+
+    with torch.no_grad():
+        outputs = model(input_tensor)
+        predicted_idx = outputs.argmax(dim=1).item()
+
+    return class_names[predicted_idx]
