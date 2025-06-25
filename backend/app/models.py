70b2dd83cfbb00832279eef11a995c971b3cfc30
Jiwei-Cao <jiweicao2006@gmail.com>
2025-06-25 14:14:51 +0100

    updated gitignore

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,16 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=datetime.now)
+
+    logs = relationship("RecyclyingLog", back_populates="user")
