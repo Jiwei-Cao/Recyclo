d5f20a4964693a20c31ebbc2e1757ccf05462f49
Jiwei-Cao <jiweicao2006@gmail.com>
2025-06-30 13:21:34 +0100

    updated UI for header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,23 +5,31 @@ export default function Header( {darkMode, setDarkMode}) {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
 
+  const headerClass = `
+    w-full px-4 py-2 flex items-center justify-between absolute top-0 left-0 z-50
+  `
+
+  const buttonClass = `
+    text-lg font-bold hover:text-green-600 transition cursor-pointer 
+  `
+
   return (
-    <header className="w-full px-4 py-2 flex items-center justify-between absolute top-0 left-0 z-50">
+    <header className={headerClass}>
       <div className="flex space-x-6">
         <button
-          className="text-lg font-bold hover:text-green-600 transition"
+          className={buttonClass}
           onClick={() => navigate("/")}
         >
           🌱 Recyclo
         </button>
         <button
-          className="text-lg hover:text-green-600 transition"
+          className={buttonClass}
           onClick={() => navigate(token ? "/stats" : "/login")}
         >
           {token ? "📊 My Stats" : "🔑 Login"}
         </button>
         <button
-          className="text-lg hover:text-green-600 transition"
+          className={buttonClass}
           onClick={() => navigate("/leaderboard")}
         >
           🏆 Leaderboard
