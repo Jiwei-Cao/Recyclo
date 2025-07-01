519703a1076bd62047beb0d006fb3e8d3f98f3cd
Jiwei-Cao <jiweicao2006@gmail.com>
2025-07-01 12:32:01 +0100

    updated header import

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Header from './components/Header';
+import Header from "./Header"
 import api from './api';
 
 function Login({ darkMode, setDarkMode}) {
