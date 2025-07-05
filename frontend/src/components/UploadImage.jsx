d7a0081972a18aee909c5f090cd68e02561e4c6b
Jiwei-Cao <jiweicao2006@gmail.com>
2025-07-05 17:24:55 +0100

    fix https error

--- frontend/src/components/UploadImage.jsx
+++ frontend/src/components/UploadImage.jsx
@@ -94,7 +94,7 @@ const UploadImage = ({ onResult, darkMode}) => {
                     <>
                         <svg
                             className="animate-spin h-5 w-5 mr-2"
-                            xmlns="http://www.w3.org/2000/svg"
+                            xmlns="https://www.w3.org/2000/svg"
                             fill="none"
                             viewBox="0 0 24 24"
                         >
